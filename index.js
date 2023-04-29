f5de7c0a7b10ae9e00276a94b461c8c8aa7f5e74
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-29 14:34:20 +0530

    Update index.js
    adding the animation effect

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ $('.btn').click(function(){
 
     userClickedPattern.push(userChosenColour);
 
+    playsound(userChosenColour);
+
+    animatePress(userChosenColour);
+
 });
 
 
@@ -28,9 +32,25 @@ function nextsequence() {
 
     $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-    var audio = new Audio('./sounds'+ randomChosenColour +'.mp3');
+    playsound(randomChosenColour);
+    
+}
+
+function playsound(name){
+//taking the name parameter from the above function where we getting the id through the btn click,
+//its the first function above.
+    var audio = new Audio('./sounds'+ name +'.mp3');
     audio.play();
 }
 
+function animatePress(currentcolour){
+    // $('.btn').click.addClass('Pressed');
+//the current colour gets picked up from the function above where we are getting
+//the btn id through press , all the elemets are taking parameter from it only.
+    $('#' + currentcolour).addClass('Pressed');
 
+    setTimeout(function(){
+        $('#' + currentcolour).removeClass('pressed');
+    },100);
+}
 
