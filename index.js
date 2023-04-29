2dfbb954da7cd254186c9d81b3babb97ca094715
DITNO <92884446+DITNO@users.noreply.github.com>
2023-04-29 15:12:51 +0530

    Update index.js
    key press function and heading change

--- index.js
+++ index.js
@@ -4,12 +4,28 @@ var gamePattern = [];
 
 var userClickedPattern = [];
 
+var started = false;
+
+var level =0;
+
+$(document).keypress(function(){
+    if(!started){
+
+    $('#level-tittle').text('Level '+level);
+
+    nextsequence();
+    started = true;
+
+    }
+});
 
 $('.btn').click(function(){
     
     var userChosenColour = $(this).attr('id');
-
+    
     userClickedPattern.push(userChosenColour);
+
+    
 
     playsound(userChosenColour);
 
@@ -19,6 +35,10 @@ $('.btn').click(function(){
 
 
 function nextsequence() {
+    
+    level++;
+    $('#level-tittle').text('Level '+level);
+
     // var n = Math.random();
     // var n = n*4;
     // n = Math.floor(n);
@@ -33,13 +53,14 @@ function nextsequence() {
     $("#"+ randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
     playsound(randomChosenColour);
+
     
 }
 
 function playsound(name){
 //taking the name parameter from the above function where we getting the id through the btn click,
 //its the first function above.
-    var audio = new Audio('./sounds'+ name +'.mp3');
+    var audio = new Audio('sounds/'+ name +'.mp3');
     audio.play();
 }
 
@@ -47,10 +68,11 @@ function animatePress(currentcolour){
     // $('.btn').click.addClass('Pressed');
 //the current colour gets picked up from the function above where we are getting
 //the btn id through press , all the elemets are taking parameter from it only.
-    $('#' + currentcolour).addClass('Pressed');
+    $('#' + currentcolour).addClass('pressed');
 
     setTimeout(function(){
         $('#' + currentcolour).removeClass('pressed');
     },100);
 }
+
 
